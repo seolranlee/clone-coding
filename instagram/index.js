a8a374efd0ddd62505889f2fbc10655687d6a40e
설설이 <leesugar90@gmail.com>
2020-07-16 13:00:03 +0900

    범용적으로 코드 수정

--- instagram/index.js
+++ instagram/index.js
@@ -1,17 +1,25 @@
 class Feed {
-  constructor() {
-    this.profile = {
-      thumb: "",
-      id: ""
-    };
-    this.likes = [];
-    this.text = "";
-    this.comments = [];
-    this.isLike = false;
-    this.isDeleted = false;
-    this.isKeeped = false;
+  constructor({
+    profile,
+    images,
+    likes,
+    text,
+    comments,
+    isLiked,
+    isDeleted,
+    isKeeped,
+    createdAt
+  }) {
+    this.profile = profile;
+    this.images = images;
+    this.likes = likes;
+    this.text = text;
+    this.comments = comments;
+    this.isLiked = isLiked;
+    this.isDeleted = isDeleted;
+    this.isKeeped = isKeeped;
 
-    this.createdAt;
+    this.createdAt = createdAt;
 
     this.$el = null;
   }
@@ -23,10 +31,10 @@ class Feed {
           <div class="header__account-info">
             <div class="user__thumb">
               <img
-                src="https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-19/s320x320/105950367_289081092275749_7298170428236645519_n.jpg?_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_ohc=eaD1prziFIoAX-0Ehdz&oh=d0e075d6cd42b6ee7dc914b1db09df08&oe=5F3594E5"
+                src="${this.profile.thumb}"
               />
             </div>
-            <a class="user__id" href="">zara</a>
+            <a class="user__id" href="">${this.profile.id}</a>
           </div>
           <div class="header__actions">
             <svg
@@ -63,7 +71,7 @@ class Feed {
         </div>
         <div class="feed__images">
           <img
-            src="https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-15/e35/p1080x1080/107159166_710665782837519_4238818945133716288_n.jpg?_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_cat=1&_nc_ohc=9SdWysUpTw0AX_OGU-p&oh=3f6f7fd5ccec45e2b79757ee2c34c776&oe=5F37ABF6"
+            src="${this.images[0]}"
           />
         </div>
         <div class="feed__icons">
@@ -131,20 +139,23 @@ class Feed {
           </div>
         </div>
         <div class="feed__likes">
-          <p class="likes__text">좋아요 <em>309</em>개</p>
+          <p class="likes__text">좋아요 <em>${this.likes.length}</em>개</p>
         </div>
         <div class="feed__text">
           <p class="text">
-            <a href="" class="account__id">zara</a>Discover the new kids
-            collection
+            <a href="" class="account__id">${this.profile.id}</a>${this.text}
           </p>
         </div>
         <div class="feed__view-comments">
-          <a href="" class="view-comment__all-comment">댓글 221개 모두 보기</a>
+          <a href="" class="view-comment__all-comment">댓글 <em>${
+            this.comments.length
+          }</em>개 모두 보기</a>
           <div class="view-comment__comment-list">
-            <div class="comment-list__comment-item">
+          ${this.comments.map(comment => {
+            return `
+              <div class="comment-list__comment-item">
               <p class="text">
-                <a href="" class="account__id">dantetexas</a>She said
+                <a href="" class="account__id">${comment.id}</a>${comment.comment}
               </p>
               <button class="comment-item__like">
                 <svg
@@ -161,29 +172,11 @@ class Feed {
                 </svg>
               </button>
             </div>
-            <div class="comment-list__comment-item">
-              <p class="text">
-                <a href="" class="account__id">mimi201674</a>What's wrong with
-                lips?
-              </p>
-              <button class="comment-item__like">
-                <svg
-                  aria-label="좋아요"
-                  class="_8-yf5 "
-                  fill="#8e8e8e"
-                  height="12"
-                  viewBox="0 0 48 48"
-                  width="12"
-                >
-                  <path
-                    d="M34.6 6.1c5.7 0 10.4 5.2 10.4 11.5 0 6.8-5.9 11-11.5 16S25 41.3 24 41.9c-1.1-.7-4.7-4-9.5-8.3-5.7-5-11.5-9.2-11.5-16C3 11.3 7.7 6.1 13.4 6.1c4.2 0 6.5 2 8.1 4.3 1.9 2.6 2.2 3.9 2.5 3.9.3 0 .6-1.3 2.5-3.9 1.6-2.3 3.9-4.3 8.1-4.3m0-3c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5.6 0 1.1-.2 1.6-.5 1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z"
-                  ></path>
-                </svg>
-              </button>
-            </div>
+            `;
+          })}
           </div>
         </div>
-        <div class="feed__time">1시간 전</div>
+        <div class="feed__time"><em>${this.createdAt}</em>시간 전</div>
         <div class="feed__write-comments">
           <div class="wirte-comment__input">
             <input
@@ -198,10 +191,44 @@ class Feed {
       `;
   }
 }
-
+const responseDate = {
+  data: [
+    {
+      profile: {
+        thumb:
+          "https://pbs.twimg.com/profile_images/923057960577675265/ccdSWBm2_400x400.jpg",
+        id: "peppoya"
+      },
+      images: [
+        "https://pbs.twimg.com/media/EAPAJcFU0AAyPnW?format=jpg&name=large"
+      ],
+      likes: [
+        {
+          thumb:
+            "https://pbs.twimg.com/profile_images/1218201600860160000/Lkzq8rKr_400x400.jpg",
+          id: "afternoon"
+        }
+      ],
+      text: "브랜디 페퍼 하이",
+      comments: [
+        {
+          thumb:
+            "https://pbs.twimg.com/profile_images/1218201600860160000/Lkzq8rKr_400x400.jpg",
+          id: "afternoon",
+          comment: "하이 최애 갱얼쥐"
+        }
+      ],
+      isLiked: true,
+      isDeleted: false,
+      isKeeped: false,
+      createdAt: 3
+    }
+  ]
+};
 const timeLine = document.getElementById("timeline");
 const feedList = [];
-for (let i = 0; i < 5; i++) {
-  feedList.push(new Feed());
-  timeLine.innerHTML += feedList[i].render();
-}
+
+responseDate.data.forEach((data, index) => {
+  feedList.push(new Feed(data));
+  timeLine.innerHTML += feedList[index].render();
+});
